f1610cfe444eabb71ba82dec193ca4c3f37da45c
Karav <ioscarhunter@gmail.com>
2015-05-16 03:13:12 +0700

    3-5

--- Main.cpp
+++ Main.cpp
@@ -60,7 +60,7 @@ int main(void) {
 //		delay(100);
 //		digitalWrite(TRIG1, LOW);
 		delay(100);
-		for(int i=4;i<6;i++){
+		for(int i=3;i<5;i++){
 			switcher.switchpin(i);
 			cout<<i<<"-"<<sensor[i].getdistance()<<endl;
 		}
