dc8e75f1f191e5859996bf69e1cbbda9316ec042
Karav <ioscarhunter@gmail.com>
2015-05-16 02:39:33 +0700

    error

--- Calibrate.cpp
+++ Calibrate.cpp
@@ -1,7 +1,7 @@
 #include "Calibrate.h"
 using namespace std;
 
-double Calibrate : dif_distance(double precise){
+double Calibrate :: dif_distance(double precise){
 	save_precise = precise;
 	detect_distance = precise - save_precise;
 	return detect_distance;
